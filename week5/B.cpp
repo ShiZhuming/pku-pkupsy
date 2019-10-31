9f3ab133db9ff15dac1673cde3eb4d6a8b705914
shizhuming <shizhuming@pku.edu.cn>
2019-10-31 11:49:32 +0800

    This problem is begin with 1, not 0.I dislike it!

--- week5/B.cpp
+++ week5/B.cpp
@@ -4,18 +4,16 @@ int main()
     int n;
     const int init = 2;
     scanf("%d",&n);
-    int little = 0, middle = 0, large = 0, born = init, sum = 0;
+    int little = init, middle = 0, large = 0, sum = 0;
     for (int i = 0; i < n; i++)
     {
-        sum = large + middle + little + born;
-        printf("%d\t%d\t%d\t%d\t%d\n",i,large,middle,little,born);
+        sum = large + middle + little;
+        // printf("%d\t%d\t%d\t%d\n",large,middle,little,sum);
         // 兔子长大
         large += middle;
         middle = little;
-        little = born;
+        little = large;
         // 生兔子
-        born = 2 * large;
-        // printf("%d\n",little);
     }
     printf("%d\n",sum/2);
     return 0;
